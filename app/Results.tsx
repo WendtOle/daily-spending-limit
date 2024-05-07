0f6edff594430c7ef463a09e94f4fd08ac5d5747
Olee <olee@Oles-Air.fritz.box>
2024-05-07 17:09:11 +0200

    Rename UI

--- app/Results.tsx
+++ app/Results.tsx
@@ -35,21 +35,21 @@ export default function Results({
           <p>{days} d</p>
         </div>
         <div className="flex justify-between">
-          <p>acutal DSL: </p>
+          <p>current DSL: </p>
           <p>{Math.floor(actualCurrentBudget / days)} €/d</p>
+        </div>
+        <div className="flex justify-between">
+          <p>ideal DSL: </p>
+          <p>{Math.floor(idealDSL)} €/d</p>{" "}
         </div>
       </div>
 
       {daysToGiveOutNothingToReturnToIdeal > 0 && (
         <div className="pt-4">
           <p className="text-center text-orange-700">
-            You are currently over your ideal DSL!
+            You have spend more than the ideal DSL!
           </p>
-          <div className="flex justify-between">
-            <p>ideal DSL: </p>
-            <p>{Math.floor(idealDSL)} €/d</p>
-          </div>
-          <p>To get back:</p>
+          <p>To reclaim the ideal DSL:</p>
           <ul className="ml-8 list-disc">
             <li>
               {Math.ceil(daysToGiveOutNothingToReturnToIdeal)} d & DSL= 0 €/d
