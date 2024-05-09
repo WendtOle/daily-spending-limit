f61e2273c7ae31452844e43a81614cfc5438cc8a
Olee <olee@Oles-Air.fritz.box>
2024-05-09 12:51:35 +0200

    fixup - prevent zooming on iOS

--- app/Input.tsx
+++ app/Input.tsx
@@ -8,13 +8,13 @@ export default function Input({ label, value, setValue }: InputProps) {
     <div className="flex flex-col mt-3">
       <label
         htmlFor={label}
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        className="block mb-2 font-medium text-gray-900 dark:text-white"
       >
         {label}
       </label>
       <input
         id={label}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-base"
+        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-base"
         type="number"
         value={value}
         onChange={(e) => setValue(+e.target.value)}
