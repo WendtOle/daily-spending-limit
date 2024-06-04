30178ccaa92091c7f844534b48cc0c504ea8ee25
Olee <olee@Oles-Air.fritz.box>
2024-06-04 17:54:22 +0200

    Adjust result information

--- app/Results.tsx
+++ app/Results.tsx
@@ -36,10 +36,11 @@ export default function Results({ currentBudget }: ResultsProps) {
     ? actualStartBudget / lastDayOfPeriod(thirdMonthMode)
     : undefined;
   const days = dayToEndOfPeriod(thirdMonthMode);
-  const daysToGiveOutNothingToReturnToIdeal = idealDSL
-    ? days - actualCurrentBudget / idealDSL
-    : undefined;
 
+  const actualCurrentDSL = actualStartBudget
+    ? (actualStartBudget - actualCurrentBudget) /
+      (lastDayOfPeriod(thirdMonthMode) - days)
+    : undefined;
   if (actualCurrentBudget < 0) {
     return null;
   }
@@ -66,32 +67,24 @@ export default function Results({ currentBudget }: ResultsProps) {
           <p>Days left: </p>
           <p>{days} d</p>
         </div>
-        <div className="flex justify-between">
-          <p>current DSL: </p>
-          <p>{Math.floor(actualCurrentBudget / days)} €/d</p>
-        </div>
+
         {idealDSL && (
           <div className="flex justify-between">
             <p>ideal DSL: </p>
             <p>{Math.floor(idealDSL)} €/d</p>{" "}
           </div>
         )}
-      </div>
-
-      {daysToGiveOutNothingToReturnToIdeal &&
-        daysToGiveOutNothingToReturnToIdeal > 0 && (
-          <div className="pt-4">
-            <p className="text-center text-orange-700">
-              You have spend more than the ideal DSL!
-            </p>
-            <p>To reclaim the ideal DSL:</p>
-            <ul className="ml-8 list-disc">
-              <li>
-                {Math.ceil(daysToGiveOutNothingToReturnToIdeal)} d & DSL= 0 €/d
-              </li>
-            </ul>
+        <div className="flex justify-between">
+          <p>you should target DSL: </p>
+          <p>{Math.floor(actualCurrentBudget / days)} €/d</p>
+        </div>
+        {actualCurrentDSL && (
+          <div className="flex justify-between">
+            <p>actual until now DSL: </p>
+            <p>{Math.floor(actualCurrentDSL)} €/d</p>
           </div>
         )}
+      </div>
     </div>
   );
 }
