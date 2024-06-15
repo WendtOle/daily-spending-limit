6a034b287b8a0826fefc2de112439044c1059819
Olee <olee@Oles-Air.fritz.box>
2024-06-15 13:49:56 +0200

    Restructure application

--- app/Input.tsx
+++ app/Input.tsx
@@ -2,23 +2,27 @@ interface InputProps {
   label: string;
   value: number | undefined;
   setValue: (newValue: number) => void;
+  inlineButton?: () => React.ReactNode;
 }
-export default function Input({ label, value, setValue }: InputProps) {
+export default function Input({
+  label,
+  value,
+  setValue,
+  inlineButton,
+}: InputProps) {
   return (
-    <div className="flex flex-col mt-3">
-      <label
-        htmlFor={label}
-        className="block mb-2 font-medium text-gray-900"
-      >
+    <div className="flex flex-col mt-3 relative">
+      <label htmlFor={label} className="block font-small text-gray-900">
         {label}
       </label>
       <input
         id={label}
-        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-base"
+        className="text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 text-base text-center shadow"
         type="number"
         value={value}
         onChange={(e) => setValue(+e.target.value)}
       />
+      <div className="absolute right-0 bottom-1">{inlineButton?.()}</div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -26,27 +26,27 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 relative">
-      <button
-        // @ts-ignore
-        popovertarget={SETTINGS_MODAL_ID}
-        className="absolute right-0 top-0 m-2 sm:m-4"
-      >
-        <FaCog fontSize={20} />
-      </button>
-      <SettingsModal />
-      <h1 className="text-3xl text-center uppercase tracking-tighter">
-        Daily spending limit (DSL)
+    <main className="flex min-h-screen flex-col items-center relative">
+      <h1 className="text-3xl text-center uppercase tracking-tighter py-4">
+        Daily spending limit
       </h1>
-      <h2 className="text-2xl mt-2">{getDate()}</h2>
-      <div className="mt-4">
+      <div className="mt-4 flex flex-col items-center space-y-2">
         <Input
           label="Current budget"
           value={currentBudget ?? 0}
           setValue={handleCurrentBudgetChange}
         />
+        <button
+          className="p-2 rounded-lg w-full border-gray-300 text-gray-900 shadow"
+          // @ts-ignore
+          popovertarget={SETTINGS_MODAL_ID}
+        >
+          Settings
+        </button>
       </div>
+
       {currentBudget && <Chart current={currentBudget} />}
+      <SettingsModal />
     </main>
   );
 }
