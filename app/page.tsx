2fb0c9cd8aaa261bda36e21962ee03f027c2f3c7
Olee <olee@Oles-Air.fritz.box>
2024-06-16 17:41:24 +0200

    Restyle main balance input

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import Input from "./Input";
 import Chart from "./Chart";
 import {
   LocalStorageKey,
@@ -9,8 +8,10 @@ import {
 } from "./localstorage";
 import { SETTINGS_MODAL_ID, SettingsModal } from "./SettingsModal";
 import { WelcomeModal } from "./WelcomeModal";
+import { FaCheck, FaCog, FaEuroSign } from "react-icons/fa";
 
 export default function Home() {
+  const [tempBalance, setTempBalance] = useState<string | undefined>();
   const [currentBudget, setCurrentBudget] = useState<number | undefined>();
 
   useEffect(() => {
@@ -28,26 +29,52 @@ export default function Home() {
     localStorage.setItem(LocalStorageKey.CURRENT_BUDGET, value.toString());
   };
 
+  const handleBalanceChange = (value: string) => {
+    setTempBalance(value);
+  };
+
+  const setBalance = () => {
+    if (tempBalance === undefined || tempBalance === "") {
+      return;
+    }
+    handleCurrentBudgetChange(+tempBalance);
+    setTempBalance(undefined);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center relative">
+    <main className="flex min-h-screen flex-col items-center relative bg-slate-50">
       <h1 className="text-3xl text-center uppercase tracking-tighter py-4">
         Daily spending limit
       </h1>
-      <div className="mt-4 flex flex-col items-center space-y-2">
-        <Input
-          label="Current budget"
-          value={currentBudget ?? 0}
-          setValue={handleCurrentBudgetChange}
+      <div className="w-80 mt-4 shadow bg-white rounded-t-2xl rounded-b relative peer">
+        <input
+          className="text-gray-900 rounded-2xl focus:outline-none block w-full text-base pl-12 py-3 shadow-md"
+          type="number"
+          placeholder="Update todays account balance"
+          onChange={(e) => handleBalanceChange(e.target.value)}
+          value={tempBalance ?? ""}
         />
-        <button
-          className="p-2 rounded-lg w-full border-gray-300 text-gray-900 shadow"
-          // @ts-ignore
-          popovertarget={SETTINGS_MODAL_ID}
-        >
-          Settings
-        </button>
-      </div>
+        <div style={{ position: "absolute", top: "14px", left: "10px" }}>
+          <FaEuroSign fontSize={20} color="gray" />
+        </div>
+        <div style={{ position: "absolute", top: "14px", right: "10px" }}>
+          {tempBalance !== undefined && tempBalance !== "" && (
+            <button onClick={setBalance}>
+              <FaCheck fontSize={20} color="gray" />
+            </button>
+          )}
+        </div>
+        <div className="flex justify-between p-4 px-8 ">
+          <div className="text-gray-500">Current balance: {currentBudget}€</div>
 
+          <button
+            // @ts-ignore
+            popovertarget={SETTINGS_MODAL_ID}
+          >
+            <FaCog fontSize={20} color="gray" />
+          </button>
+        </div>
+      </div>
       {currentBudget && <Chart current={currentBudget} />}
       <SettingsModal />
       <WelcomeModal />
