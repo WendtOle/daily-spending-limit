93c2df74dbff238a505b4bfbf5d1cbaab38169ac
Olee <olee@Oles-Air.fritz.box>
2024-06-23 14:34:31 +0200

    Restyle pending expenses list

--- app/modal/InputValuesModal.tsx
+++ app/modal/InputValuesModal.tsx
@@ -3,7 +3,7 @@ import { deletePendingEntry } from "../localstorage";
 import { useLocalstorageValues } from "../hooks/useLocalstorageValues";
 import { ConceptType, ModalType, getExplanationModalId } from "./Modals";
 import { Modal } from "../Modal";
-import { FaTrash } from "react-icons/fa6";
+import { FaRotateRight, FaTrash } from "react-icons/fa6";
 import { OpenModalButton } from "../OpenModalButton";
 import { FaQuestionCircle } from "react-icons/fa";
 
@@ -35,32 +35,48 @@ export const InputValuesModal = () => {
           <FaQuestionCircle />
         </OpenModalButton>
       </div>
-      <div className="w-full max-h-96 overflow-auto flex items-center flex-col space-y-1">
+      <div className="w-full max-h-60 overflow-auto flex items-center flex-col space-y-2">
+        <div className="flex flex-row space-x-8 items-center w-60">
+          <p>Date</p>
+          <p>Amount</p>
+          <p>Recurring</p>
+        </div>
         {pendingEntries
           .sort((a, b) => (a.clearingDay < b.clearingDay ? -1 : 1))
-          .map((entry) => (
-            <div
-              key={entry.id}
-              className="flex flex-row justify-between space-x-1 py-1 max-w-96"
-            >
-              <div className="flex flex-row bg-slate-100 space-x-1 px-4 py-1 w-60">
-                <p>{entry.value}€ </p>
-                <p>on the {entry.clearingDay}. </p>
-                <p>{entry.repeatsEveryMonth ? "every month" : ""}</p>
+          .map((entry) => {
+            const inactive = entry.clearingDay <= new Date().getDate();
+            return (
+              <div
+                key={entry.id}
+                className={`flex flex-row justify-between space-x-1 max-w-96 border-b-2 ${
+                  inactive ? "text-slate-300" : ""
+                }`}
+              >
+                <div className="flex flex-row space-x-16 items-center px-4 py-1 w-60">
+                  <p>
+                    {entry.clearingDay}.{new Date().getMonth() + 1}{" "}
+                  </p>
+                  <p>{entry.value}€ </p>
+
+                  {entry.repeatsEveryMonth && <FaRotateRight />}
+                </div>
+                <button
+                  onClick={() => deletePendingEntry(entry.id)}
+                  className=""
+                >
+                  <FaTrash className="text-slate-600" />
+                </button>
               </div>
-              <button onClick={() => deletePendingEntry(entry.id)} className="">
-                <FaTrash className="text-slate-600" />
-              </button>
-            </div>
-          ))}
-        <div className="m-2">
-          <button
-            {...addPendingEntryButtonProps}
-            className="rounded-full shadow px-4 py-1 w-full shadow w-48 mb-2"
-          >
-            Add pending entry
-          </button>
-        </div>
+            );
+          })}
+      </div>
+      <div className="w-full flex flex-row justify-center">
+        <button
+          {...addPendingEntryButtonProps}
+          className="rounded shadow px-4 py-2 w-full shadow mb-2 uppercase"
+        >
+          Add pending entry
+        </button>
       </div>
     </Modal>
   );
