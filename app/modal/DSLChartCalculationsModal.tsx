f5186790eb2d9ac156aae7b983b7a4e01c2daeb1
Olee <olee@Oles-Air.fritz.box>
2024-07-07 13:20:29 +0200

    Refactor DSL chart explanation

--- app/modal/DSLChartCalculationsModal.tsx
+++ app/modal/DSLChartCalculationsModal.tsx
@@ -4,6 +4,16 @@ import { useLocalstorageValues } from "../hooks/useLocalstorageValues";
 import { getPeriod } from "../lastDayOfMonth";
 import { ModalType } from "./Modals";
 
+interface Single {
+  upper: string;
+  lower: string;
+}
+
+interface Props {
+  block: [Single, Single] | [Single];
+  result?: string;
+}
+
 export const DSLChartCalculationsModal = () => {
   const {
     startBudget,
@@ -33,18 +43,97 @@ export const DSLChartCalculationsModal = () => {
   const leftPeriod = endPeriod - today + 1;
   const donePeriod = periodLength - leftPeriod;
 
-  interface Single {
-    upper: string;
-    lower: string;
-  }
+  const explanations: Array<{ label: string; rows: Props[] }> = [
+    {
+      label: "Ideal DSL",
+      rows: [
+        {
+          block: [
+            {
+              upper: "Spending budget",
+              lower: "Period length",
+            },
+          ],
+        },
+        {
+          block: [
+            {
+              upper: `${startBudget}€`,
+              lower: `${periodLength}d`,
+            },
+          ],
+          result: `${idealDSL}€/d`,
+        },
+      ],
+    },
+    {
+      label: "Actual DSL",
+      rows: [
+        {
+          block: [
+            {
+              upper: "Money spent",
+              lower: "Period done",
+            },
+            {
+              upper: "current - offset - pending fixed costs",
+              lower: "Period done",
+            },
+          ],
+        },
+        {
+          block: [
+            {
+              upper: `${startBudget}€ - (${currentBudget}€ - ${budgetOffset}€ - ${pendingFixedCosts}€)`,
+              lower: `${donePeriod}d`,
+            },
+            {
+              upper: `${
+                (startBudget ?? 0) -
+                ((currentBudget ?? 0) - (budgetOffset ?? 0) - pendingFixedCosts)
+              }€`,
+              lower: `${donePeriod}d`,
+            },
+          ],
+          result: `${actualDSL}€/d`,
+        },
+      ],
+    },
+    {
+      label: "Target DSL",
+      rows: [
+        {
+          block: [
+            {
+              upper: "Money left",
+              lower: "Period left",
+            },
+            {
+              upper: "current - puffer - pending fixed costs",
+              lower: "Period left",
+            },
+          ],
+        },
+        {
+          block: [
+            {
+              upper: `${currentBudget}€ - ${budgetOffset}€ - ${pendingFixedCosts}€`,
+              lower: `${leftPeriod}d`,
+            },
+            {
+              upper: `${
+                (currentBudget ?? 0) - budgetOffset - pendingFixedCosts
+              }€`,
+              lower: `${leftPeriod}d`,
+            },
+          ],
+          result: `${targetDSL}€/d`,
+        },
+      ],
+    },
+  ];
 
-  const getNew = ({
-    steps,
-    result,
-  }: {
-    steps: [Single, Single] | [Single];
-    result?: string;
-  }) => {
+  const getNew = ({ block: steps, result }: Props) => {
     return (
       <div className="flex flex-row items-center text-xs">
         {steps.map((step, index) => (
@@ -68,93 +157,23 @@ export const DSLChartCalculationsModal = () => {
   return (
     <Modal modalId={ModalType.DSL_CHART_CALCULATIONS} fullScreen>
       <h1 className="uppercase text-xl">DSL Chart Calculation</h1>
-      <div className="p-2 border rounded relative flex flex-col space-y-2 items-center">
-        <p className="text-sm absolute -bottom-4 right-0 bg-white px-2 py-1">
-          Ideal DSL
-        </p>
-        {getNew({
-          steps: [
-            {
-              upper: "Spending budget",
-              lower: "Period length",
-            },
-          ],
-        })}
-        {getNew({
-          steps: [
-            {
-              upper: `${startBudget}€`,
-              lower: `${periodLength}d`,
-            },
-          ],
-          result: `${idealDSL}€/d`,
-        })}
-      </div>
-      <div className="p-2 border rounded relative flex flex-col space-y-2 items-center">
-        <p className="text-sm absolute -bottom-4 right-0 bg-white px-2 py-1">
-          Actual DSL
-        </p>
-        {getNew({
-          steps: [
-            {
-              upper: "Money spent",
-              lower: "Period done",
-            },
-            {
-              upper: "current - offset - pending fixed costs",
-              lower: "Period done",
-            },
-          ],
-        })}
-        {getNew({
-          steps: [
-            {
-              upper: `${startBudget}€ - (${currentBudget}€ - ${budgetOffset}€ - ${pendingFixedCosts}€)`,
-              lower: `${donePeriod}d`,
-            },
-            {
-              upper: `${
-                (startBudget ?? 0) -
-                ((currentBudget ?? 0) - (budgetOffset ?? 0) - pendingFixedCosts)
-              }€`,
-              lower: `${donePeriod}d`,
-            },
-          ],
-          result: `${actualDSL}€/d`,
-        })}
-      </div>
-      <div className="p-2 border rounded relative flex flex-col space-y-2 items-center">
-        <p className="text-sm absolute -bottom-4 right-0 bg-white px-2 py-1">
-          Target DSL
-        </p>
-        {getNew({
-          steps: [
-            {
-              upper: "Money left",
-              lower: "Period left",
-            },
-            {
-              upper: "current - puffer - pending fixed costs",
-              lower: "Period left",
-            },
-          ],
-        })}
-        {getNew({
-          steps: [
-            {
-              upper: `${currentBudget}€ - ${budgetOffset}€ - ${pendingFixedCosts}€`,
-              lower: `${leftPeriod}d`,
-            },
-            {
-              upper: `${
-                (currentBudget ?? 0) - budgetOffset - pendingFixedCosts
-              }€`,
-              lower: `${leftPeriod}d`,
-            },
-          ],
-          result: `${targetDSL}€/d`,
-        })}
-      </div>
+      <>
+        {explanations.map(({ label, rows: props }, index) => (
+          <div
+            key={index}
+            className="p-2 border rounded relative flex flex-col space-y-2 items-center"
+          >
+            <p className="text-sm absolute -bottom-4 right-0 bg-white px-2 py-1">
+              {label}
+            </p>
+            <>
+              {props.map((prop, index) => (
+                <div key={index}>{getNew(prop)}</div>
+              ))}
+            </>
+          </div>
+        ))}
+      </>
     </Modal>
   );
 };
