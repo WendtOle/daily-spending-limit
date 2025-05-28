44435d83578e84b5575724f4346c41f6cd6b2fbb
Ole <ole@Mac.fritz.box>
2025-05-28 19:28:08 +0200

    Remove unused

--- app/charts/TimeChart.tsx
+++ app/charts/TimeChart.tsx
@@ -12,7 +12,6 @@ export default function Chart() {
     startBudget: start,
     currentBudget: current,
     history,
-    payedFixedCosts,
     pendingFixedCosts,
   } = useLocalstorageValues();
 
